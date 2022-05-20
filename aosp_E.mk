fcdbd133133a21f03dd4b53deee39aca10ade3d7
Pratyay Borborah <102370202+popoA3M@users.noreply.github.com>
2022-05-20 23:35:28 +0530

    E: Update raven fingerprint to May 2022

--- aosp_E.mk
+++ aosp_E.mk
@@ -55,7 +55,7 @@ TARGET_SUPPORTS_BLUR := false
 PRODUCT_GMS_CLIENTID_BASE := android-huaqin
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 S3B1.220218.004 8242181 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220505.002 8353555 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
